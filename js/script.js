cdbfcb5808aaabc4ee4e3657c93d270c894e34d4
Guido-95 <guidocarna1995@gmail.com>
2021-05-17 13:23:07 +0200

    esercitazione

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Descrizione:
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+
+
+var app = new Vue(
+    {
+        el: "#root",
+        data:{
+            message:"Messaggio Esercitazione",
+            immagine:"https://siviaggia.it/wp-content/uploads/sites/2/2017/03/paesaggio-alpi-think-1217.jpg"
+        }
+    }
+)
+
